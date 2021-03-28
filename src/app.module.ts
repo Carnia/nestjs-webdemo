5a5041ea44df1e7b2b66ea3b5e127f4fff972382
liqiang <xlqdys@foxmail.com>
2021-03-28 10:26:56 +0800

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
-import { BlogModule } from './blogs/blog.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [BlogModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
