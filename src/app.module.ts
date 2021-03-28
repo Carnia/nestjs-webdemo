ce579faef60d247f039f8823b236b3c8be3360b7
liqiang <xlqdys@foxmail.com>
2021-03-28 10:47:32 +0800

    user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserService } from './business/user/user.service';
-import { UserController } from './business/user/user.controller';
+import { UserModule } from './business/user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  imports: [UserModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
